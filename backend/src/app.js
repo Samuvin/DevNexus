41ac9c9fbf03c81391893a289c25d65d2ad44b2e
Kavya-3112 <samuvin.j@gmail.com>
2024-11-27 19:46:24 +0530

    init

--- backend/src/app.js
+++ backend/src/app.js
@@ -50,6 +50,30 @@ app.get("/feed", async (req, res) => {
 	}
 });
 
+app.delete("/user", async (req, res) => {
+	try {
+		const user = await User.findByIdAndDelete(req.body.userId);
+		if (!user) {
+			res.status(404).send("User Not Found");
+		} else res.status(200).send("User deleted successfully");
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
+app.patch("/user", async (req, res) => {
+	const userId = req.body.userId;
+	const data = req.body;
+	try {
+		const user = await User.findByIdAndUpdate({ _id: userId }, data);
+		if (!user) {
+			res.status(404).send("User Not Found");
+		} else res.status(200).send("User Updated Successfully");
+	} catch (err) {
+		res.status(400).send("Something went wrong");
+	}
+});
+
 app.listen(process.env.PORT, () => {
 	console.log("listening on", process.env.PORT);
 });
