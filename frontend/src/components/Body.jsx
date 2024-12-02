00f4239c053b777c39c3e37ce52c9513ba830915
Kavya-3112 <samuvin.j@gmail.com>
2024-12-03 01:32:30 +0530

    auth

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import { Outlet } from "react-router-dom";
+const Body = () => {
+	return (
+		<>
+			<Navbar />
+			<Outlet />
+			{/* <Footer /> */}
+		</>
+	);
+};
+
+export default Body;
---
+++ frontend/src/components/Login.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
+const Login = () => {
+	const [emailId, setEmailId] = useState("admin@gmail.com");
+	const [password, setPassword] = useState("Admin@123");
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+
+	const handleLogin = async (e) => {
+		e.preventDefault();
+		try {
+			const res = await axios.post(
+				BASE_URL + "/auth/login",
+				{
+					emailId,
+					password,
+				},
+				{ withCredentials: true }
+			);
+			dispatch(addUser(res.data));
+			return navigate("/");
+		} catch (err) {
+			console.log("Error in Login " + err.message);
+		}
+	};
+
+	return (
+		<div className="hero bg-base-200 min-h-screen">
+			<div className="hero-content flex-col lg:flex-row-reverse">
+				<div className="text-center lg:text-left">
+					<h1 className="text-5xl font-bold">Login now!</h1>
+					<p className="py-6">
+						Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+						excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+						a id nisi.
+					</p>
+				</div>
+				<div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+					<form className="card-body">
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Email</span>
+							</label>
+							<input
+								type="email"
+								placeholder="email"
+								className="input input-bordered"
+								required
+								value={emailId}
+								onChange={(e) => setEmailId(e.target.value)}
+							/>
+						</div>
+						<div className="form-control">
+							<label className="label">
+								<span className="label-text">Password</span>
+							</label>
+							<input
+								type="password"
+								placeholder="password"
+								className="input input-bordered"
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+							<label className="label">
+								<a href="#" className="label-text-alt link link-hover">
+									Forgot password?
+								</a>
+							</label>
+						</div>
+						<div className="form-control mt-6">
+							<button className="btn btn-primary" onClick={handleLogin}>
+								Login
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Login;
