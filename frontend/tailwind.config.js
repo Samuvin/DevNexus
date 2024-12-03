b94807295c785accb917eb8a3aa7354e12cf825a
Kavya-3112 <samuvin.j@gmail.com>
2024-12-03 14:49:24 +0530

    theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+
 export default {
-	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+	content: ["./public/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {}, // Leave space for custom theme extensions if needed
 	},
 	plugins: [daisyui],
+	daisyui: {
+		themes: ["night"],
+	},
 };
